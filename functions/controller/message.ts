ac95cc18f8721fbbb6692fd7c3ccac943662dbcf
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-03-10 19:15:42 -0500

    add phone_num to db at send request

--- functions/controller/message.ts
+++ functions/controller/message.ts
@@ -2,6 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import axios from 'axios';
 import validator from 'validator'
 import { logger } from '../utils/logger'
+import { v4 } from 'uuid' 
+import admin from 'firebase-admin'
+import { minutesToMilliseconds } from "date-fns";
+import { addMinutesToTimestamp, hasExpire } from "../utils/time";
+import { isEmpty, isEqual } from "lodash";
 
 export const sendMessage = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -20,6 +25,9 @@ export const sendMessage = async (req: Request, res: Response, next: NextFunctio
             return res.status(400).send({ error: 'Not a valid US phone number'})
         }
 
+        let code = Math.floor(Math.random() * 899999 + 100000); // generate 6 digit code
+        let c_id = v4();
+
         // let response = await axios.post('https://api.telnyx.com/v2/messages', {
         //     "from": "+15732241462",
         //     "to": "+19175787352",
@@ -33,8 +41,20 @@ export const sendMessage = async (req: Request, res: Response, next: NextFunctio
         // })
 
         // console.log(response.data)
+        
+        await admin.firestore().collection('/sms_verification').doc(c_id).set({
+            c_id, 
+            code,
+            expiration: addMinutesToTimestamp(15),
+            phone_num: phone_num,
+        })
 
-        res.send('success')
+        res.cookie('c_id', c_id, {
+            maxAge: minutesToMilliseconds(15),
+            httpOnly: true,
+        })
+
+        res.status(200).send();
     } catch (error) {
         if(axios.isAxiosError(error)){
             // if axios request has failed 
@@ -45,6 +65,49 @@ export const sendMessage = async (req: Request, res: Response, next: NextFunctio
         // let err = (error as AxiosError)
         // console.log(err.response?.data);
         logger.error((error as Error).message);
-        res.status(400).send({ error: 'failed'})
+        res.status(500).send({ error: 'Unable to complete the request'});
+    }
+}
+
+interface ICodeData {
+    expiration: number,
+    code: number | string,
+    c_id: string,
+    phone_num: string,
+}
+export const verifyCode = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // check for the cookie, required to check for the code in the backend
+        if(!req.cookies.c_id){
+            return res.status(401).send({ error: 'Not Authorize'});
+        }
+
+        if(!req.body.code){
+            return res.status(400).send({ error: 'No code was provided'});
+        }
+
+        let c_id: string = req.cookies.c_id;
+        let code: string | number = req.body.code;
+
+        // get the code data from the database
+        let code_data = (await admin.firestore().collection('/sms_verification').doc(c_id).get()).data() as ICodeData;
+
+        // check if the code data exist 
+        if(!code_data || isEmpty(code_data)){
+            return res.status(400).send({ error: 'Code has either expire or not found'});
+        }   
+        // check if the code data has expire or not
+        if(hasExpire(code_data.expiration)){
+            return res.status(400).send({ error: 'The code has expired' });
+        }
+
+        // compare the codes        
+        if(!isEqual(code_data.code.toString(), code.toString())){
+            return res.status(400).send({ error: 'The code does not match' })
+        }
+    
+        res.status(200).send();
+    } catch (error) {
+        res.status(400).send({ error: (error as Error).message ?? 'Failed to verify sms code'});
     }
 }
--- functions/utils/time.ts
+++ functions/utils/time.ts
@@ -2,3 +2,7 @@
 export const addMinutesToTimestamp = (minute: number) => {
     return Date.now() + (minute * 60 * 1000)
 }
+
+export const hasExpire = (time: number) => {
+    return Date.now() >= time;
+}
