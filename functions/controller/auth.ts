25584448ae86af470dcc965437956ea167ab5f1a
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-03-21 01:51:31 -0400

    complete signin sync function

--- functions/controller/auth.ts
+++ functions/controller/auth.ts
@@ -1,29 +1,50 @@
 import { NextFunction, Request, Response } from 'express'
-// import admin from 'firebase-admin'
+import admin from 'firebase-admin'
+import { isEmpty } from 'lodash';
 
 export const Signin = async (req: Request, res: Response, next: NextFunction) => {
-    console.log('login')
-    res.status(200).send('login is ran');
+    try {
+        // sync old data to the new doc
+        await admin.firestore().runTransaction(async (transaction) => {
+            const user_ref = admin.firestore().collection('/usersTest').doc(req.user.uid);
+            let user_data = transaction.get(user_ref);
 
-    // try {
-    //     // sync old data to the new doc
-    //     // admin.firestore().runTransaction(async (transaction) => {
-    //     //     const user_ref = admin.firestore().collection('/usersTest').doc(req.user.uid);
-    //     //     let user_data = transaction.get(user_ref);
+            // if the user doc (new doc) does not exist
+            if(isEmpty((await user_data).data)){
+                let old_user = (await transaction.get(user_ref.collection('customer').doc('details'))).data();
+                let old_reward = (await transaction.get(user_ref.collection('rewards').doc('points'))).data();
 
-    //     //     console.log((await user_data).exists)
-    //     //     // if the user doc (new doc) does not exist
-    //     //     if(!(await user_data).exists){
-    //     //         let old_user = transaction.get(user_ref.collection('customer').doc('details'));
-    //     //         let old_reward = transaction.get(user_ref.collection('reward').doc('points'));
+                transaction.set(user_ref, {
+                    name: old_user?.customer.name  ? old_user.customer.name :'',
+                    phone: old_user?.customer.phone ? old_user.customer.phone : '',
+                    phone_list: old_user?.verifiedNumbers ? old_user.verifiedNumbers : [],
+                    address: {
+                        address: old_user?.address.address ? old_user?.address.address : '',
+                        street:  '',
+                        city: old_user?.address.city ? old_user?.address.city : '',
+                        state: '',
+                        zipcode: old_user?.address.zipcode ? old_user?.address.zipcode : '',
+                        delivery_fee: old_user?.address.deliveryFee ? old_user.address.deliveryFee : 0,
+                        business: old_user?.address.business ?  old_user?.address.business : '',
+                        apt: old_user?.address.apt ? old_user?.address.apt : '',
 
-    //     //         console.log(old_user)
-    //     //         console.log(old_reward)
-                
-    //     //     }
-    //     // })
+                    },
+                    billings: {
+                        customer_id: old_user?.billing.customerId ? old_user.billing.customerId  :'',
+                        cards: []
+                    },
+                    reward: {
+                        points: old_reward?.point ? old_reward.point :0,
+                        transactions: []
+                    },
+                })       
+            }
+        })
 
-    // } catch (error) {
-    //     console.log(error);
-    // }
+        res.status(200).send();
+
+
+    } catch (error) {
+        res.status(400).send({ error: 'Failed to login'});
+    }
 }
