2e060078fe572edecc47e62a2d9bfbfeaa29245f
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-07-04 15:55:40 -0400

    move customer functionality to customer router

--- functions/routes/auth.ts
+++ functions/routes/auth.ts
@@ -1,29 +1,10 @@
 import express from 'express'
 import * as authController from '../controller/auth';
-import * as customerController from '../controller/customer'
 import { checkFirebaseToken } from '../middleware/auth';
 
 const auth = express.Router();
 
 // authentication
 auth.post('/login', checkFirebaseToken, authController.Signin);
-
-// CUSTOMER
-auth.get('/customer', checkFirebaseToken, customerController.getCustomerInfo);
-
-auth.patch('/customer/name', checkFirebaseToken, customerController.updateCustomerName);
-
-auth.get('/customer/order_history', checkFirebaseToken, customerController.getOrderHistory)
-
-auth.get('/customer/reward_history', checkFirebaseToken, customerController.getRewardHistory)
-
-
-// ADDRESS
-auth.post('/address/delivery', checkFirebaseToken, customerController.calculateDelivery)
-
-auth.patch('/address/apt_business', checkFirebaseToken, customerController.updateAptAndBusiness)
-
-auth.delete('/address/apt_business', checkFirebaseToken, customerController.removeAptAndBusiness)
-
 
 export default auth
