9604468eb81c5df3dd9a597d0ec58a92f4ec93f6
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-07-03 23:56:05 -0400

    update message routes

--- functions/routes/auth.ts
+++ functions/routes/auth.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import * as authController from '../controller/auth';
-import * as messageController from '../controller/message'
 import * as customerController from '../controller/customer'
 import { checkFirebaseToken } from '../middleware/auth';
 
@@ -8,13 +7,6 @@ const auth = express.Router();
 
 // authentication
 auth.post('/login', checkFirebaseToken, authController.Signin);
-
-// MESSAGE 
-auth.post('/message/send', checkFirebaseToken, messageController.sendVerificationSMS);
-
-auth.post('/message/verify', checkFirebaseToken, messageController.verifyCode);
-
-auth.get('/email/confirmation', messageController.sendContactMessage);
 
 // CUSTOMER
 auth.get('/customer', checkFirebaseToken, customerController.getCustomerInfo);
--- functions/routes/message.ts
+++ functions/routes/message.ts
@@ -1,6 +1,14 @@
 import express from 'express'
+import { checkFirebaseToken } from '../middleware/auth';
+import * as messageController  from '../controller/message'
 
 const message = express.Router();
 
+// MESSAGE 
+message.post('/sms/send', checkFirebaseToken, messageController.sendVerificationSMS);
+
+message.post('/sms/verify', checkFirebaseToken, messageController.verifyCode);
+
+message.get('/email/confirmation', messageController.sendContactMessage);
 
 export default message
