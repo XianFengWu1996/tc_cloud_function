d65c7e9dbef1b78d019822b1b1678ab3084bdd50
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-07-09 23:30:40 -0400

    remove add routes

--- functions/controller/auth.ts
+++ functions/controller/auth.ts
@@ -1,14 +1,14 @@
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import admin from 'firebase-admin'
-import { isEmpty} from 'lodash';
 
-export const Signin = async (req: Request, res: Response, next: NextFunction) => {
+// signing in for the user
+export const Signin = async (req: Request, res: Response) => {
     try {
         await admin.firestore().runTransaction(async (transaction) => {
             const user_ref = admin.firestore().collection('/usersTest').doc(req.user.uid);
             let user_data = (await transaction.get(user_ref)).data();
             
-              // if user collection does not exist
+              // if user collection does not exist, create one for the user
               if(!user_data){
                 transaction.set(user_ref, {
                     name: '',
@@ -37,7 +37,6 @@ export const Signin = async (req: Request, res: Response, next: NextFunction) =>
 
         res.status(200).send();
     } catch (error) {
-        console.log(error);
-        res.status(400).send({ error: 'ERR: Failed to login'});
+        res.status(400).send({ error: (error as Error).message ?? 'ERR: Failed to login'});
     }
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,6 @@ import { initializeApp } from "firebase-admin";
 import express from 'express'
 import * as functions from "firebase-functions";
 import store from '../routes/store'
-import admin from '../routes/admin';
 import auth from '../routes/auth';
 import payment from '../routes/payment';
 import message from '../routes/message';
@@ -16,12 +15,6 @@ import { middleware } from './config/base';
 initializeApp()
 
 export const base_app = express();
-
-
-// THE EXPRESS APP FOR ADMIN 
-const admin_app = express();
-middleware(admin_app);
-admin_app.use('/', admin);
 
 const store_app = express();
 middleware(store_app);
@@ -47,7 +40,6 @@ const customer_app = express();
 middleware(customer_app)
 customer_app.use('/', customer)
 
-exports.admin = functions.region('us-east4').https.onRequest(admin_app);
 exports.store = functions.region('us-east4').https.onRequest(store_app);
 exports.auth = functions.region('us-east4').https.onRequest(auth_app);
 exports.payment = functions.region('us-east4').https.onRequest(payment_app);
